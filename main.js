d65b6e0e3248bfdab06224dca326e15f91be844f
Noah Thomas <92344495+NoahT777@users.noreply.github.com>
2022-04-18 12:28:18 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+IMG1 = "grizzlies_img.jpeg"
+IMG2 = "warriors_img.jpeg"
+IMG3 = "bucks_img.jpg"
+
+function display(){
+
+    document.getElementById("img1").innerHTML = IMG1;
+    document.getElementById("img2").innerHTML = IMG2;
+    document.getElementById("img3").innerHTML = IMG3;
+
+}
